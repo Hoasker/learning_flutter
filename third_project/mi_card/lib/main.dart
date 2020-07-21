be0a4cfbd8e5cd581624ea793019198cfb4795eb
Artyom Shivtsov <hoasker@realzealot.com>
2020-07-21 22:57:05 +0600

    mi card

--- third_project/mi_card/lib/main.dart
+++ third_project/mi_card/lib/main.dart
@@ -1,39 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:[
               Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text("Container 1"),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text("Container 2"),
-              ),
-              Container(
-                height: 100.0,
                 width: 100.0,
                 color: Colors.red,
-                child: Text("Container 3"),
               ),
+              Container(
+                  width: 100.0,
+                  height: 200.0,
+                  color: Colors.yellow, //),
+                  padding: EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+                  child: Container(color: Colors.green)),
+              Container(width:100.0, color:Colors.blue),
             ],
           ),
         ),
